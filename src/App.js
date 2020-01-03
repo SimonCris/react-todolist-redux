34f001d24400913372dd19c832eb03a7f8982663
Simone Crisafi <simone.crisafi@nttdata.com>
2020-01-03 17:42:14 +0100

    add redux, store & reactRef

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {createStore} from "redux";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/** La funzione reducer serve per inizializzare lo store mediante il metodo createStore.
+ * E' una funzione che restituisce uno stato dell'applicazione in funzione dell'azione
+ * ricevuta. In questo caso, semplicemente restituisce una copia dello stato senza modificarlo*/
+function reducers(state = {}, action) {
+    return {...state};
+}
+
+const initTodos = [
+    'Fare la spesa',
+    'Fare i compiti',
+    'Chiamare a casa'
+    ];
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        };
+        /** Riferimento ad un elemento del DOM, in questo caso all'input di testo */
+        this.todoInputRef = React.createRef();
+    }
+
+    componentDidMount() {
+        /** Tramite il metodo createStore viene creato uno stato dell'applicazione.
+         * Il metodo prende in input la funzione reducers, in modo che venga restituito
+         * uno stato specifico in funzione dell'azione e lo stato iniziale, in questo caso
+         * l'array initTodos.
+         * Tramite getState possiamo recuperare lo stato corrente dell'applicazione*/
+        const store = createStore(reducers, {todos: [...initTodos]});
+        console.log('store', store.getState());
+        this.setState({
+            todos: [...store.getState().todos]
+        })
+    }
+
+    addToDo = () => {
+        alert(this.todoInputRef.current.value);
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    TODO LIST
+                </header>
+                <div>
+                    <input ref={this.todoInputRef} />
+                    <button onClick={this.addToDo}>Add</button>
+                </div>
+                <div>
+                    <ul>
+                        {
+                            this.state.todos.map((todo, index) => {
+                                return <li key={index}>{todo}</li>
+                            })
+                        }
+                    </ul>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
