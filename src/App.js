2fcee439ed93b1ffe6fa61b7f7b0abd86421035c
Simone Crisafi <simone.crisafi@nttdata.com>
2020-01-03 18:22:47 +0100

    add store dispatcher & subscribe

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import './App.css';
 
 /** La funzione reducer serve per inizializzare lo store mediante il metodo createStore.
  * E' una funzione che restituisce uno stato dell'applicazione in funzione dell'azione
- * ricevuta. In questo caso, semplicemente restituisce una copia dello stato senza modificarlo*/
+ * ricevuta. In questo caso, quando viene passata l'azione ADD_TODO aggiunge il nuovo valore
+ * all'array e restituisce lo stato modificato*/
 function reducers(state = {}, action) {
-    return {...state};
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                todos : [
+                   action.todoToAdd,
+                   ...state.todos
+                ]
+            };
+
+        default:
+            return {...state};
+    }
 }
 
 const initTodos = [
     'Fare la spesa',
     'Fare i compiti',
     'Chiamare a casa'
-    ];
+];
+
+/** Tramite il metodo createStore viene creato uno stato dell'applicazione.
+ * Il metodo prende in input la funzione reducers, in modo che venga restituito
+ * uno stato specifico in funzione dell'azione e lo stato iniziale, in questo caso
+ * l'array initTodos.
+ * Tramite getState possiamo recuperare lo stato corrente dell'applicazione*/
+const store = createStore(reducers, {todos: [...initTodos]});
 
 class App extends Component {
 
@@ -27,20 +46,29 @@ class App extends Component {
     }
 
     componentDidMount() {
-        /** Tramite il metodo createStore viene creato uno stato dell'applicazione.
-         * Il metodo prende in input la funzione reducers, in modo che venga restituito
-         * uno stato specifico in funzione dell'azione e lo stato iniziale, in questo caso
-         * l'array initTodos.
-         * Tramite getState possiamo recuperare lo stato corrente dell'applicazione*/
-        const store = createStore(reducers, {todos: [...initTodos]});
-        console.log('store', store.getState());
         this.setState({
             todos: [...store.getState().todos]
-        })
+        });
+
+        /** La subscribe dello store serve per catturare ogni aggiornamento dello
+         * store e svolgere delle azioni */
+        store.subscribe(() => {
+            this.setState({
+                todos: [...store.getState().todos]
+            });
+        });
     }
 
     addToDo = () => {
-        alert(this.todoInputRef.current.value);
+        const newTodoValue = this.todoInputRef.current.value;
+        /** Tramite il metodo dispatch dello store, vengono passati
+         * alla funzione reducers dello store, l'azione (type) e il valore
+         * (todoToAdd in questo caso) che intercetterà l'action e aggiungerà
+         * il valore all'array todos*/
+        store.dispatch({
+            type: 'ADD_TODO',
+            todoToAdd: newTodoValue
+        })
     };
 
     render() {
